a7028f0bbc7175af64244768416991d645ad6e6c
RSimmons2021 <fowl_137@hotmail.com>
2025-03-15 21:05:46 +0000

    Add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Juno106Synth from './components/Juno106Synth';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="app-title">React Juno-106 Synth</h1>
+      <Juno106Synth />
+    </div>
+  );
+}
+
+export default App;
